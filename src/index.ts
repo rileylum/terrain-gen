6096c5c9d150056892e3e4f6f3abb3a2ba447cf0
Riley Lum <rileylum@adam.com.au>
2025-06-12 16:06:21 +0930

    Project setup

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+// Perlin Noise Generation
+// Step 1 - Generate a 2d grid of random gradient vectors
+// Step 2 - Compute dot product between vectors and offset
+// Step 3 - Interpolate these values
+
+function createGrid(n: number) {
+  console.log("GRID")
+} 
+
+createGrid(3)
